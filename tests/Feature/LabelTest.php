e9ae4764d511ec669489333094f4bb629ae138fb
Florent Cuer <florentcuer@gmail.com>
2025-03-31 08:48:29 +0100

    created createLabel success test

---
+++ tests/Feature/LabelTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Label;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Tests\TestCase;
+
+class LabelTest extends TestCase
+{
+
+    use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_createLabel_success(): void
+    {
+        Label::factory()->create();
+
+        $testData = [
+            'name' => 'label name',
+        ];
+
+        $response = $this->postJson('api/labels', $testData);
+
+        $response->assertStatus(201)
+            ->assertJson(function (AssertableJson $json) {
+                $json->hasAll(['message', 'success']);
+            });
+
+        $this->assertDatabaseHas('labels', $testData);
+    }
+}
